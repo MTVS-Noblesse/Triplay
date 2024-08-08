4227191faa8b7a5366de818a69692d68f4102408
lsy1307 <lsy1307@inha.edu>
2024-08-08 15:52:47 +0900

    Chore: build.gradle, 기타 파일 수정

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,9 @@ import svgr from '@svgr/rollup'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 5173,
+    host: "0.0.0.0",
+  },
   plugins: [react(), svgr()],
 })
