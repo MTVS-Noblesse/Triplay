072be953090a75b7e303c9219142384fdd9b0db4
Minjae Lee <parousia0918@gmail.com>
2024-08-26 12:56:50 +0900

    #13 Chore: Post 관련 도메인 프로젝트 구조 변경
    ### 변경사항
    1. 기존에는 command, query 별로 Entity, DTO 등을 분리하였으나, 유지보수가 어려운 관계로 common 이라는 패키지 하위에 통합해서 관리하는 쪽으로 수정했어요.

--- backend/src/main/java/com/noblesse/backend/post/exception/PostReportNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.noblesse.backend.post.exception;
-
-public class PostReportNotFoundException extends IllegalArgumentException {
-    public PostReportNotFoundException(Long id) {
-        super("# 포스트 신고 고유 ID - " + id + "를 찾을 수 없어요...");
-    }
-}
